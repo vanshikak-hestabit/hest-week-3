469907d249f24dac8f31efb5883e77189656de57
Vanshika Khandelwal <vanshikak.hestabit@gmail.com>
2025-11-24 15:56:49 +0530

    warning fixed

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -19,15 +19,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-       
-          
-          {children}
-          
-       
-      </body>
-    </html>
+
+    <div>
+      {children}
+    </div>
+    
   );
 }
